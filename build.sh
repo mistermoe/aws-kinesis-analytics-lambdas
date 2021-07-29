a57bd7e60eb52725b2f959071d66dbb388b2c03c
Moe Jangda <m.jangda88@gmail.com>
2021-07-29 13:55:56 -0500

    update workflow

--- build.sh
+++ build.sh
@@ -4,6 +4,8 @@ CWD="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 
 docker build --no-cache \
     -f ${CWD}/Dockerfile \
-    -t ${ECR_REGISTRY}/${ECR_REPOSITORY}:$(date +"%Y.%m.%d.%s") \
+    -t ${ECR_REGISTRY}/${ECR_REPOSITORY}:$IMG_TAG \
     ${CWD}
+
+docker push ${ECR_REGISTRY}/${ECR_REPOSITORY}:$IMG_TAG
 

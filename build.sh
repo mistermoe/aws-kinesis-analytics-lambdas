476bf3234ad34f09c4c40f4196e19a6e49fe382e
Moe Jangda <m.jangda88@gmail.com>
2021-07-29 13:55:40 -0500

    flesh out build script

--- build.sh
+++ build.sh
@@ -4,8 +4,6 @@ CWD="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 
 docker build --no-cache \
     -f ${CWD}/Dockerfile \
-    -t 893445720768.dkr.ecr.us-east-1.amazonaws.com/kintest:2021.28.07-1 \
+    -t ${ECR_REGISTRY}/${ECR_REPOSITORY}:$(date +"%Y.%m.%d.%s") \
     ${CWD}
-
-# docker push 893445720768.dkr.ecr.us-east-1.amazonaws.com/kintest:2021.28.07-1
 
